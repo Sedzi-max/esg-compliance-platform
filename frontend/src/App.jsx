85c1b1e37b3e083f44e7435fdff67b01ef3be9ec
Dickson <sedzidickson@gmail.com>
2026-07-24 13:12:12 +0000

    updated components with the right path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ import EnergyAnalytics from './pages/EnergyAnalytics';
 import PlatformOverview from './pages/PlatformOverview';
 import EnergyDataEntry from './pages/EnergyDataEntry';
 import InsuranceDataEntry from './pages/InsuranceDataEntry';
-import MaterialityMatrix from './pages/MaterialityMatrix';
+import MaterialityMatrix from './components/MaterialityMatrix';
 
 
 // Component Imports
@@ -87,7 +87,7 @@ function App() {
                     <Route path="/campaigns" element={<ProtectedRoute allowedRoles={['Admin', 'Manager']}><SurveyCampaigns /></ProtectedRoute>} />
                     <Route path="/alignment" element={<ProtectedRoute allowedRoles={['Admin', 'Manager']}><FrameworkAlignment /></ProtectedRoute>} />
                     <Route path="/materiality-matrix" element={<ProtectedRoute allowedRoles={['Admin', 'Manager']}><MaterialityMatrix /></ProtectedRoute>} />
-                    
+
                     {/* Climate Stress Testing & Scenario Analysis Route */}
                     <Route path="/scenarios" element={<ProtectedRoute allowedRoles={['Admin', 'Manager']}><ScenarioAnalysis /></ProtectedRoute>} />
                     <Route path="/banking-analytics" element={<ProtectedRoute allowedRoles={['Admin', 'Manager']}><BankingAnalytics /></ProtectedRoute>} />
